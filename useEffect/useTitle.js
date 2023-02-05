50a65b97eb682e2ae4febcfc4e32af07b3f9634f
SEOKKAMONI <sj01048260279@gmail.com>
2023-02-05 17:57:06 +0900

    ADD :: useTitle

---
+++ useEffect/useTitle.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
+const App = () => {
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000); // 5초 뒤에 title이 Loading.. -> Home 으로 바뀜
+  return <div className="App"></div>;
+};
+
+export default App;
