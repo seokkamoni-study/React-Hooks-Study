8dc7960e4d255e58e3977fd306291caeab0401de
SEOKKAMONI <sj01048260279@gmail.com>
2023-02-06 17:14:05 +0900

    ADD :: useAxios

---
+++ useState/useAxios/App.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState, useRef } from "react";
+import useAxios from "./useAxios";
+
+const App = () => {
+  const { loading, data, refetch } = useAxios({
+    url: "https://yts.am/api/v2/list_movies.json",
+  });
+  return (
+    <div className="App">
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>refetch</button>
+    </div>
+  );
+};
+
+export default App;
---
+++ useState/useAxios/useAxios.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import defaultAxios from "axios";
+
+const useAxios = (options, axiosInstance = defaultAxios) => {
+  const [state, setState] = useState({
+    loading: true,
+    error: null,
+    data: null,
+  });
+  const [trigger, setTrigger] = useState(0);
+  if (!options.url) {
+    return;
+  }
+  const refetch = () => {
+    setState({
+      ...state,
+      loading: true,
+    });
+    setTrigger(Date.now());
+  };
+  useEffect(() => {
+    axiosInstance(options).then((data) => {
+      setState({
+        ...state,
+        loadding: false,
+        data,
+      }).catch((error) => {
+        setState({ ...state, loading: false, error });
+      });
+    });
+  }, [trigger]);
+  return { ...state, refetch };
+};
+
+export default useAxios;
